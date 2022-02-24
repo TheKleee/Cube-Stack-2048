b4bfd5b36142c1927afbc6133cee3c1ea13da175
TheKleee <mk.kleee@gmail.com>
2022-02-24 13:49:36 +0100

    a

--- Assets/_Game/Scripts/GameController.cs
+++ Assets/_Game/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Tabtale.TTPlugins;
 
 public class GameController : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class GameController : MonoBehaviour
     public static GameController instance;
     private void Awake()
     {
+        TTPCore.Setup();
         instance = this;
     }
     #endregion
